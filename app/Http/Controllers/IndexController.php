74d6e392e015acfba17ea25d86a20dccb1ccc47e
Vlad Bulgakov <48432102+VPLEV23@users.noreply.github.com>
2023-01-01 20:05:46 +0200

    SetUp DB + SetUp Laravel + Vue

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function index(){
+        return inertia(
+            'Index/Index', 
+            [
+                'message' => 'Hello from laravel'
+            ]
+        );
+    }
+    public function show(){
+        return inertia('Index/Show');
+    }
+}
---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>{{ message }}</div>
+    <Link href="/hello">Show Page</Link>
+</template>
+
+<script setup>
+import { Link } from "@inertiajs/inertia-vue3";
+defineProps({
+    message: String,
+});
+</script>
+<!-- <script>
+import MainLayout from "../../Layouts/MainLayout.vue";
+export default {
+    layout: MainLayout,
+};
+</script> -->
