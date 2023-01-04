457e19201f1fb02565d0f69acc58407679067a05
Vlad Bulgakov <48432102+VPLEV23@users.noreply.github.com>
2023-01-04 14:12:32 +0200

    File Uploading

---
+++ app/Http/Controllers/RealtorListingImageController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+use App\Models\ListingImage;
+
+class RealtorListingImageController extends Controller
+{
+    public function create(Listing $listing)
+    {
+        return inertia(
+            'Realtor/ListingImage/Create',
+            ['listing'=>$listing]
+        );
+    }
+    public function store(Listing $listing, Request $request)
+    {
+        if ($request->hasFile('images')) {
+            foreach ($request->file('images') as $file) {
+                $path = $file->store('images', 'public');
+
+                $listing->images()->save(new ListingImage([
+                    'filename' => $path
+                ]));
+            }
+        }
+
+        return redirect()->back()->with('success', 'Images uploaded!');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
 use App\Http\Controllers\RealtorListingController;
+use App\Http\Controllers\RealtorListingImageController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserAccountController;
 
@@ -46,4 +47,7 @@ Route::prefix('realtor')
   Route::resource('listing', RealtorListingController::class)
     ->only(['index', 'destroy', 'edit', 'update', 'create', 'store'])
     ->withTrashed();
+
+    Route::resource('listing.image', RealtorListingImageController::class)
+    ->only(['create', 'store']);
 });
