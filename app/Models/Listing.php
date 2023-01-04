3ca0be96c8e0a4981f2a86abff09ca54d4ba3861
Vlad Bulgakov <48432102+VPLEV23@users.noreply.github.com>
2023-01-04 11:59:23 +0200

    My Account Section

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -6,12 +6,17 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Listing extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = ['beds', 'baths', 'area', 'city', 'code', 'street', 'street_nr', 'price'];
+
+    protected $sortable = [
+        'price', 'created_at'
+    ];
 
     public function owner(): BelongsTo
     {
@@ -42,6 +47,15 @@ class Listing extends Model
         )->when(
             $filters['areaTo'] ?? false,
             fn ($query, $value) => $query->where('area', '<=', $value)
+        )->when(
+            $filters['deleted'] ?? false,
+            fn ($query, $value) => $query->withTrashed()
+        )->when(
+                $filters['by'] ?? false,
+                fn ($query, $value) =>
+                !in_array($value, $this->sortable)
+                    ? $query :
+                    $query->orderBy($value, $filters['order'] ?? 'desc')
         );
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\RealtorListingController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserAccountController;
 
@@ -17,20 +18,32 @@ use App\Http\Controllers\UserAccountController;
 |
 */
 
-Route::get('/',[IndexController::class, 'index']);
+Route::get('/', [IndexController::class, 'index']);
 Route::get('/hello', [IndexController::class, 'show'])
   ->middleware('auth');
 
 Route::resource('listing', ListingController::class)
-  ->only(['create', 'store', 'edit', 'update', 'destroy'])
-  ->middleware('auth');
-Route::resource('listing', ListingController::class)
-  ->except(['create', 'store', 'edit', 'update', 'destroy']);
-  Route::get('login', [AuthController::class, 'create'])
+  ->only(['index', 'show']);
+
+Route::get('login', [AuthController::class, 'create'])
   ->name('login');
+
 Route::post('login', [AuthController::class, 'store'])
   ->name('login.store');
 Route::delete('logout', [AuthController::class, 'destroy'])
-->name('logout');
+  ->name('logout');
 Route::resource('user-account', UserAccountController::class)
   ->only(['create', 'store']);
+Route::prefix('realtor')
+  ->name('realtor.')
+  ->middleware('auth')
+  ->group(function () {
+    Route::name('listing.restore')
+    ->put(
+      'listing/{listing}/restore',
+      [RealtorListingController::class, 'restore']
+    )->withTrashed();
+  Route::resource('listing', RealtorListingController::class)
+    ->only(['index', 'destroy', 'edit', 'update', 'create', 'store'])
+    ->withTrashed();
+});
